439be89abe7ea562e59db01de553a53296d0b27d
Vy Trinh <vy.trinh@cobit-solutions.com>
2026-05-15 09:44:02 +0000

    fact_daily_weather model

--- models/dim_date.sql
+++ models/dim_date.sql
@@ -1,21 +1,13 @@
-with CTE as (
+with cte as (
     select
-    to_timestamp(started_at) as started_at,
-    date(to_timestamp(started_at)) as date_started_at,
-    hour(to_timestamp(started_at)) as hour_started_at,
-    case
-        when dayofweek(to_timestamp(started_at)) in (0, 6) then 'Weekend'
-        else 'Business Day'
-    end as day_type,
-    case
-        when month(to_timestamp(started_at)) in (12, 1, 2) then 'Winter'
-        when month(to_timestamp(started_at)) in (3, 4, 5) then 'Spring'
-        when month(to_timestamp(started_at)) in (6, 7, 8) then 'Summer'
-        else 'Autumn'
-    end as season_of_year
+        to_timestamp(started_at) as started_at,
+        date(to_timestamp(started_at)) as date_started_at,
+        hour(to_timestamp(started_at)) as hour_started_at,
+        {{my_new_project.get_day_type('started_at')}} as day_type,
+        {{my_new_project.get_season('started_at')}} as season_of_year
     from {{ source('demo', 'bike') }}
     where started_at != 'started_at'
 )
 
 select *
-from CTE
+from cte
